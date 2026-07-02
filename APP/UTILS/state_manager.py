1b196673b46c686e41ac210757f44f4a6861ca58
Harshpreet Kaur <harsh5661sly@gmail.com>
2026-07-02 15:14:18 +0530

    Implement AppStateManager for emergency state tracking
    Added AppStateManager class to manage emergency shutdown state.

--- APP/UTILS/state_manager.py
+++ APP/UTILS/state_manager.py
@@ -1,1 +1,13 @@
+class AppStateManager:
+    """Global atomic storage tracks software-managed emergency trips."""
+    
+    # Track hardware emergency stop status outside database configurations
+    EMERGENCY_SHUTDOWN_ACTIVE = False
 
+    @classmethod
+    def trigger_estop(cls):
+        cls.EMERGENCY_SHUTDOWN_ACTIVE = True
+
+    @classmethod
+    def reset_estop(cls):
+        cls.EMERGENCY_SHUTDOWN_ACTIVE = False
