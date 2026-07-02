b46304ea29b9f0abc90133eda67759c5d421b794
Harshpreet Kaur <harsh5661sly@gmail.com>
2026-07-02 15:53:18 +0530

    Add dark theme configuration in app_theme.dart

--- FRONTEND/THEME/app_theme.dart
+++ FRONTEND/THEME/app_theme.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import '../utils/constants.dart';
 
+class AppTheme {
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: AppColors.bgDark,
+      colorScheme: const ColorScheme.dark(
+        primary: AppColors.accentNeon,
+        surface: AppColors.surfaceDark,
+        error: AppColors.criticalNeon,
+      ),
+      cardTheme: CardTheme(
+        color: AppColors.surfaceDark,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: const BorderSide(color: AppColors.borderDark, width: 1),
+        ),
+      ),
+    );
+  }
+}
