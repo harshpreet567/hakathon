95d52c2d4be6a8636255e5f101a3e5226cdbbab8
Harshpreet Kaur <harsh5661sly@gmail.com>
2026-07-02 15:22:48 +0530

    Add configuration settings for sensors and intervals
    Purpose: Centralizes pin assignments, telemetry thresholds, and timing intervals to make changing hardware layouts easy.

--- ARDUINO/PulseEdgeNode/config.h
+++ ARDUINO/PulseEdgeNode/config.h
@@ -1,1 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// --- HARDWARE PIN ASSIGNMENTS ---
+#define PIN_TEMP_SENSOR    A0   // Analog input for Temperature Sensor (LM35 / NTC Thermistor)
+#define PIN_CURRENT_SENSOR A1   // Analog input for Current Sensor (ACS712)
+#define PIN_PIR_MOTION     2    // Digital input for PIR Motion Sensor
+#define PIN_RELAY          3    // Digital output to control the Relay Module
+
+// --- CALIBRATION PARAMETERS ---
+#define ADC_RESOLUTION     1023.0
+#define SYSTEM_VOLTAGE     5.0
+#define CURRENT_SENSITIVITY 0.185 // Sensitivity for ACS712-05B (185mV/A)
+
+// --- TIMING INTERVALS (Milliseconds) ---
+#define INTERVAL_TELEMETRY 1000   // Read and transmit sensor data every 1 second
+#define INTERVAL_HEARTBEAT 5000   // Send status heartbeat every 5 seconds
+
+#endif
